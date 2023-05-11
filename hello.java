1ce41b1d87b953db00457935a4add7e409a95e4a
mamathahv <129505019+mamathahv@users.noreply.github.com>
2023-05-10 23:42:57 -0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,7 @@
-class hello
+class hello{
     public static void main(string[args){
 	    system.out.println("hello world")
             syatem.out.println("hello java")
     }
-inchara
-harshitha
-dhanushree   
+}
+  
